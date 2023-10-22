e991163e6ec11d38e5650e807fa4b55aab64ee4f
ErosSunda <148714612+ErosSunda@users.noreply.github.com>
2023-10-22 16:37:41 -0300

    Add files via upload

---
+++ JAVA/Script.js
@@ -0,0 +1,256 @@
+function C01() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Red.jpg" class="imagen1">
+                <p class="texto">Una red de computadoras, también llamada red de ordenadores o red informática, es un conjunto de equipos conectados por medio de cables, señales, ondas o cualquier otro método de transporte de datos, que comparten información (archivos), recursos (CD-ROM, impresoras, etc.)</p>
+            </div>
+        `;
+}
+function C02() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Topologias.png" class="imagen2">
+                <p class="texto">La topología de red física es la ubicación de diversos componentes de una red. Los diferentes conectores representan los cables de red físicos y los nodos representan los dispositivos de red físicos (como los switches). La topología de red lógica ilustra, en el nivel más alto, cómo fluyen los datos dentro de una red.</p>
+            </div>
+        `;
+}
+function C03() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Tipos.jpg" class="imagen2">
+                <p class="texto">Dependiendo del tamaño de la red esta puede clasificarse segun varios tipos</p>
+            </div>
+        `;
+}
+function C04() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Disp.jpg" class="imagen2">
+                <p class="texto">Los dispositivos de red o dispositivos de conexión son los equipos físicos necesarios para la interacción y comunicación del hardware de una red informática. Se utilizan para conectar un ordenador y otros dispositivos para que puedan compartir archivos y recursos.</p>
+            </div>
+        `;
+}
+function C05() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Router.jpg" class="imagen1">
+                <p class="texto">Un router, enrutador, o encaminador es un dispositivo que permite interconectar redes con distinto prefijo en su dirección IP. Su función es la de establecer la mejor ruta que destinará a cada paquete de datos para llegar a la red y al dispositivo de destino.</p>
+            </div>
+        `;
+}
+function C06() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Switch.jpg" class="imagen1">
+                <p class="texto"> Un switch de red o conmutador es un dispositivo de interconexión que sirve para conectar todos los equipos en una red; incluidos los computadores, las consolas, las impresoras y los servidores. Junto con el cableado forman lo que se conoce como red de área local (LAN)</p>
+            </div>
+        `;
+}
+function C07() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/AP.jpg" class="imagen2">
+                <p class="texto">Técnicamente, los AP o WAP (Access Point o Wireless Access Point) son conocidos por establecer una conexión inalámbrica entre equipos y pueden formar una red inalámbrica externa (local o internet) para interconectar dispositivos móviles o tarjetas de red inalámbricas.</p>
+            </div>
+        `;
+}
+function C08() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Server.jpg" class="imagen2">
+                <p class="texto">Los servidores de red son potentes ordenadores o dispositivos que se utilizan como depósito central para proveer diversos recursos compartidos, como espacio en disco, acceso a hardware, acceso a impresoras, servicio de correo electrónico, etc., a otros ordenadores de la red.</p>
+            </div>
+        `;
+}
+function C09() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/IP.jpg" class="imagen2">
+                <p class="texto">Una dirección IP es una etiqueta numérica que identifica de manera lógica y jerárquica a una interfaz —habitualmente un dispositivo — conectada a la red, que utilice el protocolo de internet o que corresponda al nivel de red del modelo TCP/IP.</p>
+            </div>
+        `;
+}
+function C10() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/IP.jpg" class="imagen2">
+                <p class="texto">Una dirección IP es una etiqueta numérica que identifica de manera lógica y jerárquica a una interfaz —habitualmente un dispositivo — conectada a la red, que utilice el protocolo de internet o que corresponda al nivel de red del modelo TCP/IP.</p>
+            </div>
+        `;
+}
+function C11() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/IP.jpg" class="imagen2">
+                <p class="texto">Diferencias entre tipos de ip segun su uso, rango y tipo</p>
+            </div>
+        `;
+}
+function C13() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Direcc.jpg" class="imagen2">
+                <p class="texto">Uso de Submetting y VLSM para calcular las direcciones ip existentes</p>
+            </div>
+        `;
+}
+function C12() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Clases.jpg" class="imagen1">
+                <p class="texto">Diferencia el uso de las direcciones ip dependiendo de la cantidad de host disponibles dentro del rango</p>
+            </div>
+        `;
+}
+function C15() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Protocolos.png" class="imagen2">
+                <p class="texto">En informática y telecomunicación, un protocolo de comunicaciones es un sistema de reglas que permiten que dos o más entidades de un sistema de comunicación se comuniquen entre ellas para transmitir información por medio de cualquier tipo de variación de una magnitud física.</p>
+            </div>
+        `;
+}
+function C14() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Protocolos.png" class="imagen2">
+                <p class="texto">Un protocolo de comunicación es un conjunto de normas, pautas o instrucciones que sirven para guiar las acciones durante el intercambio de información. A su vez, es necesario su cumplimiento para que un programa o una máquina las ejecute de manera correcta y organizada.</p>
+            </div>
+        `;
+}
+function C16() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Protocolos.png" class="imagen2">
+                <p class="texto">Diferentes protocolos para diferentes necesidades</p>
+            </div>
+        `;
+}
+function C18() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/CMD.jpg" class="imagen2">
+                <p class="texto">Comandos que permiten visualizar informacion del sistema operativo</p>
+            </div>
+        `;
+}
+function C19() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/CMD.jpg" class="imagen2">
+                <p class="texto">Comandos que permiten visualizar informacion del sistema operativo</p>
+            </div>
+        `;
+}
+function C20() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/CMD.jpg" class="imagen2">
+                <p class="texto">Tipos de comandos de CMD</p>
+            </div>
+        `;
+}
+function C21() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/CMD.jpg" class="imagen2">
+                <p class="texto">Muestra y manipula informacion de la configuracion ip</p>
+            </div>
+        `;
+}
+function C22() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/CMD.jpg" class="imagen2">
+                <p class="texto">Envia paquetes de datos hacia el resto de la red</p>
+            </div>
+        `;
+}
+function C23() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/CMD.jpg" class="imagen2">
+                <p class="texto">Muestra Las estadistica del dispositivo y sus puertos</p>
+            </div>
+        `;
+}
+function C24() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Protocolos.png" class="imagen2">
+                <p class="texto">Los ordenadores de una red pueden utilizar software y hardware muy diferentes; sin embargo, el uso de protocolos les permite comunicarse entre sí.</p>
+            </div>
+        `;
+}
+function C25() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Enrut.png" class="imagen2">
+                <p class="texto">El enrutamiento o ruteo es la función de buscar un camino entre todos los posibles en una red de paquetes cuyas topologías poseen una gran conectividad.</p>
+            </div>
+        `;
+}
+function C26() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Estat.jpg" class="imagen2">
+                <p class="texto">En el enrutamiento estático, un administrador de red utiliza tablas estáticas para configurar y seleccionar manualmente las rutas de red. El enrutamiento estático es útil en situaciones en las que se espera que el diseño o los parámetros de la red permanezcan constantes.En el enrutamiento estático, un administrador de red utiliza tablas estáticas para configurar y seleccionar manualmente las rutas de red. El enrutamiento estático es útil en situaciones en las que se espera que el diseño o los parámetros de la red permanezcan constantes.</p>
+            </div>
+        `;
+}
+function C27() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/Estat.jpg" class="imagen2">
+                <p class="texto">El ruteo dinámico es el proceso de planear la ruta de entrega de productos y mercancías, con el fin de que esta se haga de un modo efectivo. Funciona mediante una herramienta que genera el flujo de datos, con algoritmos que ayudan a verificar alteraciones en la vía, tal como lo hace el GPS.</p>
+            </div>
+        `;
+}
+function C28() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/DHCP.png" class="imagen2">
+                <p class="texto">El protocolo DHCP (Protocolo de configuración dinámica de host) o también conocido como «Dynamic Host Configuration Protocol«, es un protocolo de red que utiliza una arquitectura cliente-servidor.</p>
+            </div>
+        `;
+}
+function C29() {
+    const contenido = document.getElementById('Introduccion');
+    contenido.innerHTML = `
+            <div class="Contenido">
+                <img src="./IMG/Indice/NAT.jpg" class="imagen2">
+                <p class="texto">La traducción de direcciones de red, también llamado enmascaramiento de IP o NAT, es un mecanismo utilizado por routers IP para cambiar paquetes entre dos redes que asignan mutuamente direcciones incompatibles. Consiste en convertir, en tiempo real, las direcciones utilizadas en los paquetes transportados.</p>
+            </div>
+        `;
+}
+
+function mOut() {
+    document.getElementById("Introduccion").innerHTML = ""
+}
